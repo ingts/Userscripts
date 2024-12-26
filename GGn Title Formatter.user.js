4567085d7fcce36e80f2c069fb23c3424a84ad1a
ingts <165401152+ingts@users.noreply.github.com>
2024-12-26 23:51:23 +0800

    replacement order

--- GGn Title Formatter.user.js
+++ GGn Title Formatter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GGn Title Formatter
 // @namespace    none
-// @version      28
+// @version      29
 // @description  Formats title, sets alias if applicable and has buttons to undo. Adds buttons in edit page to format name and alias. Exposes title case function to other scripts
 // @author       ingts
 // @match        https://gazellegames.net/upload.php
@@ -45,7 +45,7 @@ globals.toTitleCase = function (str, alias) {
 
     return str
         .replace(/\s/g, ' ').replace('—', ' - ')
-        .replace(/ ~ /, ': ').replace(/ ~/, ': ').replace(/~$/, '').replace(/ ~$/, '').replace(/ - /, ': ').replace(/ -/, ': ').replace(/-$/, '').replace(/^-/, '')
+        .replace(/~$/, '').replace(/ ~$/, '').replace(/-$/, '').replace(/^-/, '').replace(/ ~ /, ': ').replace(/ ~/, ': ').replace(/ - /, ': ').replace(/ -/, ': ')
         .replace('™', '').replace('®', '')
         .toLowerCase().trim()
         .split(wordSeparators)
